c317084248e3cd548f2ee2758b18615e41976cda
zhy <3026144289@qq.com>
2024-01-28 20:37:34 +0800

    获取查询字符串

--- bookmanager03/book/views.py
+++ bookmanager03/book/views.py
@@ -15,4 +15,31 @@ def create_book(request):
 
 # 接收路径中的参数，参数名称不能错，参数位置可以改变
 def shop(request, shop_id, city_id):
-    return HttpResponse(f"城市id：{city_id} 商店id：{shop_id}")
+    query_params = request.GET
+    print(query_params)
+    # order = query_params['order']
+    # order = query_params.get('order')
+    # print(order)
+
+    # <QueryDict: {'order': ['readcount']}>
+    # QueryDict 具有字典的特性
+    # 还具有 一键多值
+    # <QueryDict: {'order': ['readcount', 'commentcount']}>
+    # 多值时 get 和 关键字 只能得到最后一个值
+    # order = query_params.get('order')
+    # order = query_params['order']
+    # 可以得到一个列表
+    order = query_params.getlist('order')
+    print(order)
+
+    content = f"城市id：{city_id}---商店id：{shop_id}---query_params：{order}"
+    return HttpResponse(content)
+
+###############查询字符串#####################
+"""
+查询字符串
+http://ip:port/path/path?key1=value1&key2=value2
+url 以 ? 分割为两部分
+? 前面为 请求路径
+? 后边为 查询字符串 查询字符串 类似于字典 key=value 多个数据用 & 拼接
+"""
