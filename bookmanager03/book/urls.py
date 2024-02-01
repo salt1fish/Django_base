ceaa46210357d6cceaa334416c8bef6c7c58910b
zhy <3026144289@qq.com>
2024-02-01 18:11:29 +0800

    自定义验证转换器

--- bookmanager03/book/urls.py
+++ bookmanager03/book/urls.py
@@ -1,6 +1,26 @@
 from django.urls import path
 from book.views import create_book, shop, register, json, method
 from django.urls import converters
+from django.urls import register_converter
+
+
+# 1. 定义转换器
+class MobileConverter:
+    regex = '1[3-9]\d{9}'
+
+    # 验证没有问题的数据，给视图函数
+    def to_python(self, value):
+        return int(value)
+
+    # # 将匹配结果用于反向解析传值时使用（了解）
+    # def to_url(self, value):
+    #     return str(value)
+
+
+# 2. 先注册转换器，才能在第三步中使用
+# converter 转换器类
+# type_name 转换器名字
+register_converter(MobileConverter, 'phone')
 
 urlpatterns = [
     path('create/', create_book),
@@ -8,7 +28,7 @@ urlpatterns = [
     # path('<city_id>/<shop_id>/', shop),
     # <转换器名字：变量名>
     # 转换器会对变量数据进行 正则的验证
-    path('<int:city_id>/<shop_id>/', shop),
+    path('<int:city_id>/<phone:shop_id>/', shop),
     path('register/', register),
     path('json/', json),
     path('method/', method),
